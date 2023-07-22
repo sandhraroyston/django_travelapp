e0045c0c6893cbb7b6616f9cf83f63118a0a2618
sandhraroyston <sandhraroyston@gmail.com>
2023-07-22 19:10:47 +0530

    firstcommit

--- travelproject/settings.py
+++ travelproject/settings.py
@@ -75,12 +75,18 @@ WSGI_APPLICATION = 'travelproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'traveldb',
+#         'USER': 'root',
+#         'PASSWORD': '',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'traveldb',
-        'USER': 'root',
-        'PASSWORD': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
